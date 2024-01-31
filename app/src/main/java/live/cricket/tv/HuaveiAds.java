f37b48dc588ecabcb8053da94d4d2984da953c76
jjveerg <154468632+jjveerg@users.noreply.github.com>
2024-02-01 02:04:49 +0500

    first commit

---
+++ app/src/main/java/live/cricket/tv/HuaveiAds.java
@@ -0,0 +1,15 @@
+package live.cricket.tv;
+
+import android.app.Application;
+
+import com.huawei.hms.ads.HwAds;
+
+public class HuaveiAds extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Initialize the HUAWEI Ads SDK.
+        HwAds.init(this);
+    }
+
+}
---
+++ app/src/main/java/live/cricket/tv/huawei.java
@@ -0,0 +1,15 @@
+package live.cricket.tv;
+
+
+import android.app.Application;
+
+import com.huawei.hms.ads.HwAds;
+
+public class huawei extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Initialize the Petal Ads SDK.
+        HwAds.init(this);
+    }
+}
